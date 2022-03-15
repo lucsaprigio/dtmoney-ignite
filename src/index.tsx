b670744c635adcd3a706841db30bd63ed4894e3c
Lucas Aprigio <lucsaprigio@hotmail.com>
2022-03-15 14:55:52 -0300

    Modal

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createServer } from 'miragejs';
 import { App } from './App';
+
+// Servidor com Mirage
+createServer({
+  routes() {
+    //Nome da Api
+    this.namespace = 'api';
+
+    // nome da Rota com retornando os dados.
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'Food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.render(
   <React.StrictMode>
